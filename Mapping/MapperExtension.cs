d73bd334d840aa03a0fb13ea2e97e5b67de02d6c
Ortiz <ortizdcj@gmail.com>
2023-03-14 00:08:51 -0400

    add: map functionality to 'MapperExtension' class

--- Mapping/MapperExtension.cs
+++ Mapping/MapperExtension.cs
@@ -1,6 +1,13 @@
+using System.Text.Json;
+
 namespace Mapping;
 
-public class MapperExtension
+public static class MapperExtension
 {
-	
+	#region static methods
+
+	public static T MapTo<T>(this object value) => 
+		JsonSerializer.Deserialize<T>(JsonSerializer.Serialize(value));
+
+	#endregion
 }
