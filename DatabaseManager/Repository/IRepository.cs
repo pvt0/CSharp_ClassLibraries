11aeafecebeabf256443fd4f147a9ece3311b3ea
Ortiz <ortizdcj@gmail.com>
2023-03-14 03:03:42 -0400

    add: 'Repository' generic class

--- DatabaseManager/Repository/IRepository.cs
+++ DatabaseManager/Repository/IRepository.cs
@@ -1,19 +1,19 @@
 using System.Linq.Expressions;
 using DatabaseManager.Entities;
-using Paging.Collection;
 
 namespace DatabaseManager.Repository;
 
 public interface IRepository<TEntity> where TEntity : BaseEntity
 {
-	Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> expression);
+	Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> expression, CancellationToken cancellationToken);
 	IEnumerable<TEntity> List<TId>(IEnumerable<TId>? entity = null);
-	Task<TEntity?> GetByIdAsync<TId>(TId id);
-	TEntity Insert(TEntity entity);
-	void InsertRange(IEnumerable<TEntity> entities);
+	Task<TEntity?> GetByIdAsync<TId>(TId id, CancellationToken cancellationToken);
+	Task<TEntity> InsertAsync(TEntity entity, CancellationToken cancellationToken);
+	Task InsertRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken);
 	TEntity Update(TEntity entity);
+	void UpdateRange(IEnumerable<TEntity> entities);
 	void Delete(TEntity entity);
-	void Delete<TId>(TId id);
+	Task DeleteAsync<TId>(TId id, CancellationToken cancellationToken);
 	void DeleteRange(IEnumerable<TEntity> entities);
 	void DeleteByIdRange<TId>(IEnumerable<TId> ids);
 }
---
+++ DatabaseManager/Repository/Repository.cs
@@ -0,0 +1,77 @@
+using System.Linq.Expressions;
+using DatabaseManager.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseManager.Repository;
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+{
+	#region variables
+
+	private readonly DbSet<TEntity> _dbSet;
+
+	#endregion
+
+	#region constructors
+
+	public Repository(DbContext context)
+	{
+		_dbSet = context.Set<TEntity>();
+	}
+
+	#endregion
+
+	#region IRepository implementation
+
+	public IEnumerable<TEntity> List<TId>(IEnumerable<TId>? entity = null)
+		=> _dbSet.Where(e => entity == null || entity.Contains((TId)e.Id));
+	
+	public async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> expression, CancellationToken cancellationToken)
+		=> await _dbSet.SingleOrDefaultAsync(expression, cancellationToken: cancellationToken);
+
+	public async Task<TEntity?> GetByIdAsync<TId>(TId id, CancellationToken cancellationToken)
+		=> await _dbSet.SingleOrDefaultAsync(e => e.Id == (object)id, cancellationToken: cancellationToken);
+
+	public async Task<TEntity> InsertAsync(TEntity entity, CancellationToken cancellationToken)
+	{
+		var result = await _dbSet.AddAsync(entity, cancellationToken);
+		return result.Entity;
+	}
+
+	public async Task InsertRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
+		=> await _dbSet.AddRangeAsync(entities, cancellationToken);
+
+	public TEntity Update(TEntity entity)
+	{
+		var result = _dbSet.Update(entity);
+		return result.Entity;
+	}
+
+	public void UpdateRange(IEnumerable<TEntity> entities)
+		=> _dbSet.UpdateRange(entities);
+
+	public void Delete(TEntity entity)
+		=> _dbSet.Remove(entity);
+
+	public async Task DeleteAsync<TId>(TId id, CancellationToken cancellationToken)
+	{
+		var entity = await _dbSet.FindAsync(id, cancellationToken);
+		
+		if (entity == null)
+			throw new Exception($"Does not exist {nameof(entity)} with id {id}");
+		
+		Delete(entity);
+	}
+
+	public void DeleteRange(IEnumerable<TEntity> entities)
+		=> _dbSet.RemoveRange(entities);
+
+	public void DeleteByIdRange<TId>(IEnumerable<TId> ids)
+	{
+		var entities = List(ids);
+
+		DeleteRange(entities);
+	}
+
+	#endregion
+}
