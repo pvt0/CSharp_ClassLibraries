17457c1ee21a56119666e5ca8f168a2173fe7089
Carlos J. Ortiz <ortizdcj@gmail.com>
2023-06-09 22:09:38 -0400

    delete data annotation

--- src/DatabaseManager/EntityTemplate/BaseEntity.cs
+++ src/DatabaseManager/EntityTemplate/BaseEntity.cs
@@ -1,10 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace DatabaseManager.EntityTemplate;
 
 public abstract class BaseEntity<TId>
 {
 #pragma warning disable CS8618
-	[Key] public TId Id { get; set; }
+	public TId Id { get; set; }
 #pragma warning restore CS8618
 }
